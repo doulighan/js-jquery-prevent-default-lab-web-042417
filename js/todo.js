9fd5402db1eb4a148ad95d4f5c2a6a5c8a990aa1
doulighan <doulighan@gmail.com>
2017-06-09 10:18:58 -0400

    done

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-  // call functions here
-});
+  submitForm()
+})
+
+
+function submitForm(){
+  $('form').on('submit', function(event) { 
+    var item = $('#item').val()
+    $('ol').append(`<li>${item}</li>`)
+    event.preventDefault()
+    })
+}
+
 
 // define functions here
